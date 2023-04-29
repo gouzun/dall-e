8b03db8152dacbecea94058c70377de80e0463d5
gouzun <gouzun1@gmail.com>
2023-04-29 17:59:53 +0800

    test444

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -12,8 +12,8 @@ const router = express.Router();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_key: process.env.CLOUDINARY_CLOUD_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
 });
 
 router.route('/').get(async (req, res) => {
@@ -26,36 +26,53 @@ router.route('/').get(async (req, res) => {
 });
 
 router.route('/').post(async (req, res) => {
-    console.log("Req ",req)
     try {
-        const parser = new datauri();
-        
-        console.log("postroute");
-        const { name, prompt, photo } = req.body;
-        const buffer = Buffer.from(photo.replace(/^data:image\/\w+;base64,/, ''), 'base64');
-
-        console.log("uploadeding");
-        // const photoUrl = await cloudinary.uploader.upload(photo);
-        cloudinary.uploader.upload_stream((error, result) => {
-            if (error) {
-              console.log(error);
-              res.status(500).json({ success: false, message: 'Unable to upload image to Cloudinary' });
-            } else {
-              console.log(result.secure_url);
-              
-              res.status(200).json({ success: true, data: newPost });
-            }
-            
-          }).end(buffer);
-          const newPost = await Post.create({
-            name,
-            prompt,
-            photo: result.secure_url,
-          });
-          console.log("done uploadeding");
+      const { name, prompt, photo } = req.body;
+      const photoUrl = await cloudinary.uploader.upload(photo);
+  
+      const newPost = await Post.create({
+        name,
+        prompt,
+        photo: photoUrl.url,
+      });
+  
+      res.status(200).json({ success: true, data: newPost });
     } catch (err) {
-        res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+      res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
     }
-});
+  });
+
+// router.route('/').post(async (req, res) => {
+//     console.log("Req ",req)
+//     try {
+//         const parser = new datauri();
+        
+//         console.log("postroute");
+//         const { name, prompt, photo } = req.body;
+//         const buffer = Buffer.from(photo.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+
+//         console.log("uploadeding");
+//         // const photoUrl = await cloudinary.uploader.upload(photo);
+//         cloudinary.uploader.upload_stream((error, result) => {
+//             if (error) {
+//               console.log(error);
+//               res.status(500).json({ success: false, message: 'Unable to upload image to Cloudinary' });
+//             } else {
+//               console.log(result.secure_url);
+              
+//               res.status(200).json({ success: true, data: newPost });
+//             }
+            
+//           }).end(buffer);
+//           const newPost = await Post.create({
+//             name,
+//             prompt,
+//             photo: result.secure_url,
+//           });
+//           console.log("done uploadeding");
+//     } catch (err) {
+//         res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+//     }
+// });
 
 export default router;
